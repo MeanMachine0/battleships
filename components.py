54bae4396a5fc84ef123872a50a34d4622b49749
MeanMachineMarc <marcus9981@icloud.com>
2023-11-13 23:42:26 +0000

    .

--- components.py
+++ components.py
@@ -1,5 +1,6 @@
 """Contains functions to setup the components of the game."""
 import random
+import json
 
 def initialise_board(size=10) -> list[list[None]]:
     """Returns a list of board states.
@@ -27,40 +28,35 @@ print(create_battleships(), '\n')
 
 def place_battleships(board: list[list[None]],
                       ships: dict[int],
-                      placement='simple') -> list[list[str or None]]:
-    """Returns a board with battleships placed on it."""
-    if placement == 'simple':
-        for index, (name, size) in enumerate(ships.items()):
-            board[index][:size] = [name] * size
-    elif placement == 'random':
-        while len(ships) > 0:
-            name = random.choice(list(ships.keys()))
-            size = ships[name]
-            possible_placements = []
-            for i in range(len(board)):
-                for i1 in range(len(board) - size + 1):
-                    if board[i][i1:i1 + size] == [None] * size:
-                        possible_placements.append((i, i1, 'v'))
-                    if [board[i2][i] for i2 in range(i1, i1 + size)] == [None] * size:
-                        possible_placements.append((i1, i, 'h'))
-            place_here = random.choice(possible_placements)
-            if place_here[2] == 'v':
-                board[place_here[0]][place_here[1]:place_here[1] + size] = [name] * size
-            else:
-                for column_index in range(place_here[0], place_here[0] + size):
-                    board[column_index][place_here[1]] = name
-            del ships[name]
+                      algorithm='simple') -> list[list[str or None]]:
+    """Returns a board with battleships placed on it, using
+    algorithms: simple/random/custom."""
+    match algorithm:
+        case 'simple':
+            for index, (name, size) in enumerate(ships.items()):
+                board[index][:size] = [name] * size
+        case 'random':
+            while len(ships) > 0:
+                name = random.choice(list(ships.keys()))
+                size = ships[name]
+                possible_placements = []
+                for i, column in enumerate(board):
+                    for i1 in range(len(board) - size + 1):
+                        if column[i1:i1 + size] == [None] * size:
+                            # places upwards from (i, i1)
+                            possible_placements.append((i, i1, 'v'))
+                        if [board[i2][i] for i2 in range(i1, i1 + size)] == [None] * size:
+                            # places rightwards from (i1, i):
+                            possible_placements.append((i1, i, 'h')) 
+                algorithm = random.choice(possible_placements)
+                if algorithm[2] == 'v':
+                    board[algorithm[0]][algorithm[1]:algorithm[1] + size] = [name] * size
+                else:
+                    for column_index in range(algorithm[0], algorithm[0] + size):
+                        board[column_index][algorithm[1]] = name
+                del ships[name]
+        case 'custom':
+            pass # do json serialization here
     return board
 
 print(place_battleships(initialise_board(), create_battleships(), 'random'), '\n')
-
-
-# elif placement == 'random':
-#         while len(ships) > 0:
-#             battleship_name_to_place = list(ships.keys())[randint(0, len(ships) - 1)]
-#             vertical_coordinate = randint(0, 9)
-#             horizontal_coordinate = randint(0, 9)
-#             board[vertical_coordinate][horizontal_coordinate] = battleship_name_to_place
-#             ships[battleship_name_to_place] -= 1
-#             if ships[battleship_name_to_place] == 0:
-#                 del ships[battleship_name_to_place]
--- components_tests.py
+++ components_tests.py
@@ -1,30 +1,40 @@
 """Tests for components.py"""
 import components
+import pytest
+import random
 
-random_board = components.place_battleships(
-    board=components.initialise_board(),
-    ships=components.create_battleships(),
-    algorithm='random'
-)
-
-def test_random_place_battleships_sizes():
+@pytest.mark.parametrize("seed", range(999))
+def test_random_battleships_sizes(seed):
     """Checks whether the correct number of each battleship
     is on the board using the random algorithm"""
     ships_checker = components.create_battleships()
     ships_test = {
         ship_name: 0 for ship_name in list(ships_checker.keys())
     }
+    random.seed(seed)
+    random_board = components.place_battleships(
+        board=components.initialise_board(),
+        ships=components.create_battleships(),
+        algorithm='random'
+    )
     for column in random_board:
         for value in column:
             if value is not None:
                 ships_test[value] += 1
     assert ships_test == ships_checker
 
-def test_random_place_battleships_adjacent():
+@pytest.mark.parametrize("seed", range(999))
+def test_random_battleships_adjacent(seed):
     """Checks whether each battleship placement is adjacent
     using the random algorithm"""
     ships = components.create_battleships()
     ships_found = []
+    random.seed(seed)
+    random_board = components.place_battleships(
+        board=components.initialise_board(),
+        ships=components.create_battleships(),
+        algorithm='random'
+    )
     for column_index, column in enumerate(random_board):
         for row_index, ship in enumerate(column):
             if ship is not None and ship not in ships_found:
@@ -33,14 +43,10 @@ def test_random_place_battleships_adjacent():
                 if column_index < len(random_board) - 1 and row_index < len(random_board) - 1:
                     ship_above = column[row_index + 1]
                     if ship_above == ship:
-                        for i in range(1, size):
-                            assert column[row_index + i] == ship
+                        assert column[row_index + 1:row_index + size] == [ship] * (size - 1)
                     else:
-                        for i in range(size):
-                            assert random_board[column_index + i][row_index] == ship
+                        assert [random_board[column_index + i][row_index] for i in range(size)] == [ship] * size
                 elif column_index < len(random_board) - 1:
-                    for i in range(1, size):
-                        assert random_board[column_index + i][row_index] == ship
+                    assert [random_board[column_index + i][row_index] for i in range(1, size)] == [ship] * (size - 1)
                 else:
-                    for i in range(1, size):
-                        assert column[row_index + i] == ship
+                    assert column[row_index + 1:row_index + size] == [ship] * (size - 1)
