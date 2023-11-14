acc952b2e2b0d9d0873a59083e874d505645e63e
MeanMachineMarc <marcus9981@icloud.com>
2023-11-14 18:51:30 +0000

    functional simple game loop

--- game_engine.py
+++ game_engine.py
@@ -17,27 +17,42 @@ def attack(coordinates: (int, int),
     return False
 
 def cli_coordinates_input() -> (int, int):
-    """Recieves, processes, and returns coordinates.""" # needs improving
+    """Recieves, processes, and returns coordinates."""
     while True:
         raw_input = input("Enter the coordinates you would like to attack: ")
-        horizontal_coordinate = int(raw_input.strip().split(',')[0]) - 1
-        vertical_coordinate = int(raw_input.strip().split(',')[1]) - 1
-        attack_coordinates = (horizontal_coordinate, vertical_coordinate)
-        return attack_coordinates
+        stripped_input = raw_input.strip()
+        first_char = stripped_input[0]
+        trailing_chars = stripped_input[1:]
+        if not first_char.isalpha() or not trailing_chars.isnumeric():
+            print('Invalid coordinates: please enter a letter followed by a number, e.g. A1')
+        else:
+            horizontal_coordinate = ord(first_char.upper()) - 65 # ord('A') returns 65
+            vertical_coordinate = int(trailing_chars) - 1
+            attack_coordinates = (horizontal_coordinate, vertical_coordinate)
+            return attack_coordinates
 
 def simple_game_loop():
     """Play by yourself, with no opponent attacking you."""
-    with open('ascii.txt', 'r', encoding='utf-8') as file:
+    with open('ascii/battleships.txt', 'r', encoding='utf-8') as file:
         print(f'\n{file.read()}\n\n')
     board = components.initialise_board()
     ships = components.create_battleships()
+    ships_copy = ships.copy()
     components.place_battleships(board, ships)
+    ships = ships_copy
     while len(ships) > 0:
         attack_coordinates = cli_coordinates_input()
-        hit = attack(attack_coordinates, board, ships)
-        print('Hit!' if hit else 'Missed!')
-        for column in board:
-            print(column)
-    print('GAME OVER')
+        if attack_coordinates[0] >= len(board) or attack_coordinates[1] >= len(board):
+            print('Invalid coordinates: out of range.')
+        else:
+            ships_before_attack = list(ships.keys())
+            hit = attack(attack_coordinates, board, ships)
+            print('Hit!' if hit else 'Missed!')
+            for i, ship in enumerate(ships_before_attack):
+                if len(ships) == 0 or i == len(ships) or ship != list(ships.keys())[i]:
+                    print(f'{ship} sunk!')
+                    break
+    with open('ascii/game_over.txt', 'r', encoding='utf-8') as file:
+        print(f'\n{file.read()}\n\n')
 
 simple_game_loop()
