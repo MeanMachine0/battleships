5719aae7727e22d555eed572aed1fab9b4449a4f
MeanMachineMarc <marcus9981@icloud.com>
2023-11-15 12:26:22 +0000

    renaming

--- game_engine.py
+++ game_engine.py
@@ -7,12 +7,12 @@ def attack(coordinates: (int, int),
     """Checks whether there is a ship at the given coordinates.
     If there is a ship at said coordinates, the ship is set to None on the board,
     and the dict value of said ship is decremented."""
-    value_at_coordinates = board[coordinates[0]][coordinates[1]]
-    if value_at_coordinates is not None:
+    value_at_coords = board[coordinates[0]][coordinates[1]]
+    if value_at_coords is not None:
         board[coordinates[0]][coordinates[1]] = None
-        battleships[value_at_coordinates] -= 1
-        if battleships[value_at_coordinates] == 0:
-            del battleships[value_at_coordinates]
+        battleships[value_at_coords] -= 1
+        if battleships[value_at_coords] == 0:
+            del battleships[value_at_coords]
         return True
     return False
 
@@ -37,16 +37,14 @@ def simple_game_loop():
         print(f'\n{file.read()}\n\n')
     board = components.initialise_board()
     ships = components.create_battleships()
-    ships_copy = ships.copy()
     components.place_battleships(board, ships)
-    ships = ships_copy
     while len(ships) > 0:
-        attack_coordinates = cli_coordinates_input()
-        if attack_coordinates[0] >= len(board) or attack_coordinates[1] >= len(board):
+        attack_coords = cli_coordinates_input()
+        if attack_coords[0] >= len(board) or attack_coords[1] >= len(board):
             print('Invalid coordinates: out of range.')
         else:
             ships_before_attack = list(ships.keys())
-            hit = attack(attack_coordinates, board, ships)
+            hit = attack(attack_coords, board, ships)
             print('Hit!' if hit else 'Missed!')
             for i, ship in enumerate(ships_before_attack):
                 if len(ships) == 0 or i == len(ships) or ship != list(ships.keys())[i]:
