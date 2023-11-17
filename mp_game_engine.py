e32a2ca4b4d56227f682d9e6b5c9106e5066ec12
MeanMachineMarc <marcus9981@icloud.com>
2023-11-17 15:16:15 +0000

    spacing and ascii representation of player's board

--- mp_game_engine.py
+++ mp_game_engine.py
@@ -26,12 +26,6 @@ def generate_attack() -> (int, int):
     players['ai']['possible_attacks'].remove(attack_coords)
     return attack_coords
 
-# def generate_attack() -> (int, int):
-#     """Returns coordinates to attack the player."""
-#     attack_col = random.choice(players['you'])
-#     attack_row_index = random.choice(attack_col).index
-#     return random.choice(possible_attacks)
-
 def ai_opponent_game_loop() -> None:
     """Play against the computer."""
     with open('ascii/welcome.txt', 'r', encoding='utf-8') as welcome:
@@ -41,6 +35,7 @@ def ai_opponent_game_loop() -> None:
         ships=None,
         algorithm='custom'
     )
+    board_size = len(players['you']['board'])
     for col in players['you']['board']:
         for value in col:
             if value is not None:
@@ -50,12 +45,15 @@ def ai_opponent_game_loop() -> None:
                     players['you']['ships'][value] += 1
     players['ai']['ships'] = players['you']['ships'].copy()
     players['ai']['board'] = components.place_battleships(
-        board=components.initialise_board(len(players['you']['board'])),
+        board=components.initialise_board(board_size),
         ships=players['you']['ships'].copy(),
         algorithm='random'
     )
     if players['you'] is None or players['ai'] is None:
         return
+    col_names = [str(i + 1) for i in range(board_size)]
+    col_names.insert(0, ' ')
+    col_names_string = ' '.join(col_names)
     while len(players['you']['ships']) > 0 and len(players['ai']['ships']) > 0:
         valid_attack = False
         while not valid_attack:
@@ -65,14 +63,21 @@ def ai_opponent_game_loop() -> None:
                 players['ai']['ships'],
                 attack_coords
                 )
-        print('Opponent is attacking...')
+        print('\nOpponent is attacking...')
         attack_coords = generate_attack()
         game_engine.process_attack(
             players['you']['board'],
             players['you']['ships'],
             attack_coords
         )
-        print(f'{chr(i)} ' for i in range(65, 65 + len(players['you']['board'])))
+        print(f'\n{col_names_string}')
+        for i in range(board_size):
+            row = [chr(65 + i)]
+            for i1 in range(board_size):
+                row.append('-' if players['you']['board'][i1][i] is None else 'X')
+            row_string = ' '.join(row)
+            print(row_string)
+        print('\n')
     with open('ascii/game_over.txt', 'r', encoding='utf-8') as game_over:
         print(f'\n\n{game_over.read()}\n\n\n')
     if len(players['you']['ships']) == 0:
