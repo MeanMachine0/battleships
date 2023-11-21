c74f4f6f7beb9574431a97a91e3cfcdb5809ce7b
MeanMachineMarc <marcus9981@icloud.com>
2023-11-21 02:29:28 +0000

    Refactoring

--- mp_game_engine.py
+++ mp_game_engine.py
@@ -5,18 +5,18 @@ import game_engine
 
 players = {
     'you': {
-        'ships': components.create_battleships(),
-        'board': components.initialise_board()
+        'ships': {},
+        'board': []
     },
     'ai': {
         'ships': {},
-        'board': {},
+        'board': [],
         'possible_attacks': []
     }
 }
 
 def generate_attack() -> (int, int):
-    """Returns coordinates to attack the player."""
+    """Returns the coordinates used to attack the player."""
     if len(players['ai']['possible_attacks']) == 0:
         board_size = len(players['you']['board'])
         for y in range(board_size):
@@ -30,9 +30,10 @@ def ai_opponent_game_loop() -> None:
     """Play against the computer."""
     with open('ascii/welcome.txt', 'r', encoding='utf-8') as welcome:
         print(f'\n{welcome.read()}\n\n')
+    players['you']['ships'] = components.create_battleships()
     players['you']['board'] = components.place_battleships(
         board=components.initialise_board(),
-        ships=components.create_battleships(),
+        ships=players['you']['ships'].copy(),
         algorithm='custom'
     )
     if players['you']['board'] is None:
@@ -44,8 +45,6 @@ def ai_opponent_game_loop() -> None:
         ships=players['you']['ships'].copy(),
         algorithm='random'
     )
-    if players['you'] is None or players['ai'] is None:
-        return
     col_names = [str((i + 1) % 10) for i in range(board_size)]
     col_names.insert(0, ' ')
     col_names_string = ' '.join(col_names)
@@ -73,11 +72,24 @@ def ai_opponent_game_loop() -> None:
             row_string = ' '.join(row)
             print(row_string)
         print('\n')
-    with open('ascii/game_over.txt', 'r', encoding='utf-8') as game_over:
-        print(f'\n\n{game_over.read()}\n\n\n')
-    if len(players['you']['ships']) == 0:
-        with open('ascii/you_lost.txt', 'r', encoding='utf-8') as you_lost:
-            print(f'{you_lost.read()}\n\n')
-    else:
-        with open('ascii/you_won.txt', 'r', encoding='utf-8') as you_won:
-            print(f'{you_won.read()}\n\n')
+    print(game_over(len(players['ai']['ships']) == 0))
+
+def game_over(won: bool):
+    """Returns a game over message."""
+    with open('ascii/game_over.txt', 'r', encoding='utf-8') as game_over_f:
+        message = f'\n\n{game_over_f.read()}\n\n\n'
+        if won:
+            with open('ascii/you_won.txt', 'r', encoding='utf-8') as you_won:
+                message += f'{you_won.read()}\n\n'
+        else:
+            with open('ascii/you_lost.txt', 'r', encoding='utf-8') as you_lost:
+                message += f'{you_lost.read()}\n\n'
+    return message
+
+if __name__ != '__main__':
+    players['you']['ships'] = components.create_battleships()
+    players['you']['board'] = components.place_battleships(
+        board=components.initialise_board(),
+        ships=players['you']['ships'].copy(),
+        algorithm='custom'
+    )
